cf2d86ca656ea9370f4a00e2462e6b2cd20d6b06
Pablo <pablogcg05@gmail.com>
2023-04-28 23:39:56 +0200

    Creación suma

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Buenas, el siguiente código le pedirá dos números con el proposito de sumarlos, restarlos, multiplicarlos y dividirlos entre sí y en el orden que usted de.")
+primer_numero = int(input())
+segundo_numero = int(input())
+
+print("El resultado de sumar", primer_numero,
+      "y", segundo_numero, "es:", primer_numero + segundo_numero)
