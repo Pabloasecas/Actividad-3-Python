afe51134f8e4066910336c58a4ef744af8da9b85
Pablo <pablogcg05@gmail.com>
2023-04-28 23:46:57 +0200

    Solución con el resultado directamente en el print

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 print("Buenas, el siguiente código le pedirá dos números con el proposito de sumarlos, restarlos, multiplicarlos y dividirlos entre sí y en el orden que usted de.")
-primer_numero = int(input())
-segundo_numero = int(input())
+primer_numero = float(input())
+segundo_numero = float(input())
 
 print("El resultado de sumar", primer_numero,
       "y", segundo_numero, "es:", primer_numero + segundo_numero)
+
+print("El resultado de restar", primer_numero,
+      "menos", segundo_numero, "es:", primer_numero - segundo_numero)
+
+print("El resultado de multiplicar", primer_numero,
+      "y", segundo_numero, "es:", primer_numero * segundo_numero)
+
+print("El resultado de dividir", primer_numero,
+      "entre", segundo_numero, "es:", primer_numero / segundo_numero)
